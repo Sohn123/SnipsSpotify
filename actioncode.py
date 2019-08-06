58309b991abeb5ef8d15291e79e188fe1a8c5801
Alexander Sohn <alexander.sohn642@gmail.com>
2019-08-06 17:27:17 +0200

    actioncode wurde hinzugefügt

---
+++ actioncode.py
@@ -0,0 +1,25 @@
+import paho.mqtt.client as mqtt
+import json
+
+
+def on_connect(client, userdata, flags, rc):
+    client.subscribe("hermes/intent/#")
+def play(client, userdata, msg):
+    data = json.loads(msg.payload.decode("utf-8"))
+    session_id = data['sessionId']
+    MM2('play', {})
+
+def MM2(intentname, action):
+    mqtt_client.publish(('external/MagicMirror2/HideShowMove/' + intentname),
+                        json.dumps(action))
+def say(session_id, text):
+    mqtt_client.publish('hermes/dialogueManager/endSession',
+                        json.dumps({'text': text, "sessionId": session_id}))
+
+
+client = mqtt.Client()
+
+if __name__ == "__main__":
+         client.on_connect = on_connect
+         client.message_callback_add("hermes/intent/sohn:play/#", play)
+
