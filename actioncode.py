59bd05978bd7974cc8aea0578987bc5a0d8fdc14
Alexander Sohn <alexander.sohn642@gmail.com>
2019-08-07 21:19:08 +0200

    erste funktionierende Version

--- actioncode.py
+++ actioncode.py
@@ -7,12 +7,39 @@ def on_connect(client, userdata, flags, rc):
 def play(client, userdata, msg):
     data = json.loads(msg.payload.decode("utf-8"))
     session_id = data['sessionId']
-    MM2('play', {})
     say(session_id, "der Song wird fortgesetzt")
-
+    MM2("SPOTIFY_PLAY", {"uris": ["spotify:track:4iV5W9uYEdYUVa79Axb7Rh", "spotify:track:1301WleyT98MSxVHPZCA6M"]})  
+def search(client, userdata, msg):
+    data = json.loads(msg.payload.decode("utf-8"))
+    session_id = data['sessionId']
+    slots = {slot['slotName']: slot['value']['value'] for slot in data['slots']}
+    print(slots)
+    query = '{"type": "'
+    i = 0
+    for key in slots:
+        i = i + 1
+        query = query + key 
+        if i < len(slots):
+           query = query + ","
+    query = query + '", "query":"'
+    i = 0
+    for key in slots:
+       i = i + 1
+       query = query + slots[key] 
+       if i < len(slots):
+           query = query + "+"
+    query = query + '", "random": "false"}'
+    print(query)
+    MM2search("SPOTIFY_SEARCH", query)
+def MM2search(intentname, action):
+    mqtt_client.publish(("external/MagicMirror2/Spotify/" + intentname),
+                        action)
+    print(json.dumps(action))
 def MM2(intentname, action):
-    mqtt_client.publish(('external/MagicMirror2/HideShowMove/' + intentname),
+    mqtt_client.publish(("external/MagicMirror2/Spotify/" + intentname),
                         json.dumps(action))
+    print(json.dumps(action))
+
 def say(session_id, text):
     mqtt_client.publish('hermes/dialogueManager/endSession',
                         json.dumps({'text': text, "sessionId": session_id}))
@@ -22,7 +49,8 @@ mqtt_client = mqtt.Client()
 
 if __name__ == "__main__":
          mqtt_client.on_connect = on_connect
-         mqtt_client.message_callback_add("hermes/intent/test12345:play/#", play)
+         mqtt_client.message_callback_add("hermes/intent/sohn:play/#", play)
+         mqtt_client.message_callback_add("hermes/intent/sohn:playResource/#", search)
          mqtt_client.connect("localhost", 1883)
          mqtt_client.loop_forever()
 
